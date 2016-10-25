f09284459aeb5f51615bd4f3bd2c8a099b243fb7
Georgiy Odisharia <math.kraut.cat@gmail.com>
2016-10-26 01:40:18 +0300

    Fix sfdisk functions

--- mk_sdboot.sh
+++ mk_sdboot.sh
@@ -307,7 +307,7 @@ function repartition_sd_recovery {
 		sudo umount $mnt
 	done
 
-	echo "Remove partition table..."                                                
+	echo "Remove partition table..."
 	sudo su -c "dd if=/dev/zero of=$DEVICE bs=512 count=1 conv=notrunc"
 
 	sudo sfdisk -u M $DEVICE <<-__EOF__
@@ -348,16 +348,16 @@ function repartition_sd_boot {
 		sudo umount $mnt
 	done
 
-	echo "Remove partition table..."                                                
+	echo "Remove partition table..."
 	sudo su -c "dd if=/dev/zero of=$DEVICE bs=512 count=1 conv=notrunc"
 
-	sudo sfdisk -o START,SIZE -uS $DEVICE <<-__EOF__
-	$SKIP_BOOT_SIZE,{$BOOT_SIZE}
-	,{$MODULE_SIZE}M
-	,{$ROOTFS_SIZE}M
-	,
-	,{$DATA_SIZE}M
-	,{$USER_SIZE}M
+	sudo sfdisk $DEVICE <<-__EOF__
+	${SKIP_BOOT_SIZE}M,${BOOT_SIZE}M,0xE,*
+	$(($SKIP_BOOT_SIZE+$BOOT_SIZE))M,${MODULE_SIZE}M,,-
+	$(($SKIP_BOOT_SIZE+$BOOT_SIZE+$MODULE_SIZE))M,${ROOTFS_SIZE}M,,-
+	$(($SKIP_BOOT_SIZE+$BOOT_SIZE+$MODULE_SIZE+$ROOTFS_SIZE))M,,E,-
+	,${DATA_SIZE}M,,-
+	,${USER_SIZE}M,,-
 	__EOF__
 
 	if [ "$BOOT_PART_TYPE" == "vfat" ]; then
@@ -440,4 +440,3 @@ fi
 fuse_images
 
 rm -rf $TARGET_DIR
-
